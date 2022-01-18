4bd5e55df1b93dcb216887050cb26625c8db216d
Toshi <t0324s@gmail.com>
2022-01-19 00:57:29 +0900

    add ABC232 B written in C#

---
+++ ABC/ABC232/B/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = Console.ReadLine();
+        var t = Console.ReadLine();
+        var k = (t[0] - s[0] + 26) % 26;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (k != (t[i] - s[i] + 26) % 26)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+        }
+        Console.WriteLine("Yes");
+    }
+}
